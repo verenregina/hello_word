99567585434094d208921396406da697b2962c9a
verenregina <reginaveren62@gmail.com>
2025-09-30 22:37:26 +0700

    langkah 1-praktikum 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_word/basic_widgets/text_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -104,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Veren Regina Tirsya'),
+            const MyTextWidget(),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
